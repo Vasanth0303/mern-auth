d53204f48ed762abc4b9b67765a32643547a7387
Your Name <your.email@example.com>
2025-12-29 22:45:21 +0530

    fix appcontext api import path

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useEffect, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
 import api from "../api.js";
 
 export const AppContext = createContext();
